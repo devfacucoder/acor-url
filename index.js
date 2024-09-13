7f2b9e62e504e4328b70916f733b6c1f584e51cc
facundo <facudoer@gmail.com>
2024-09-13 20:03:41 -0300

    segond commit

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-import express from "express"; // Corrección de "epxress" a "express"
-import { customAlphabet } from "nanoid";
-import mongoose from "./src/db.js"; // Asegúrate de que este archivo esté correctamente configurado
+import express from "express";
+import mongoose from "./src/db.js";
 import urlRoutes from "./src/url.routes.js";
+import cors from "cors";
+import { config } from "dotenv";
+
+config();
+
 const app = express();
+
+// Lista de orígenes permitidos
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+// Opciones de CORS
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  // Permitir solicitudes de preflight para métodos y encabezados personalizados
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 204 // algunos navegadores antiguos fallan en 204 con CORS
+};
+
+// Aplicar middleware de CORS a todas las rutas
+app.use(cors(corsOptions));
+
+// Middleware para parsear JSON
 app.use(express.json());
+
+// Aplicar las rutas
 app.use("/", urlRoutes);
+
 // Definir el puerto correctamente
 const PORT = process.env.PORT || 3000;
-
-app.post("/sendurl", (req, res) => {
-  res.send("hello");
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor abierto en el puerto ${PORT}`);
